896f056b8819e19f8492979f6ed98ed561cf5d40
zubeyranwar <zubeyranwar@gmail.com>
2024-05-04 18:20:16 +0300

    feat:changing the font to dm sans font

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,23 @@
 import './globals.css'
 import Nav from './components/Nav'
-// import { DM_Sans } from 'next/font/google'
-import Footer from './components/Footer'
-import { icons } from 'react-icons'
-
+import { DM_Sans } from 'next/font/google'
+import Footer from './components/Footer';
 
 export const metadata = {
   title: 'Startup Landing Page',
   description: 'Startup Landing Page',
-  icons:{
-    icon:'/logo-test.ico'
-  }
+  
 }
+const font = DM_Sans({
+  weight:["400","700"],
+  subsets:['latin']
+})
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body>
+      <body className={font.className}>
         <Nav />
         {children}
         <Footer />
